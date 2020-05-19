45cd64d445729e52e78349d09015bbb9f3f2bbab
Aaron Cox <aaron@greymass.com>
2020-05-19 18:02:59 -0400

    Better response testing

--- src/Response.js
+++ src/Response.js
@@ -1,118 +1,103 @@
 import React, { Component } from 'react'
-import { Header, Segment } from 'semantic-ui-react';
+import { Header, List, Segment } from 'semantic-ui-react';
 import ReactJson from 'react-json-view';
 import { CodeBlock, monokai } from 'react-code-blocks'
 
+import { JsonRpc, Api, Serialize } from 'eosjs'
+const transactionAbi = require('eosjs/src/transaction.abi.json')
+const transactionTypes: Map<string, Serialize.Type> = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), transactionAbi)
 
 class Transaction extends Component {
+  testResponse = () => {
+    const { response } = this.props
+    const tests = [
+      {
+        name: "Not Broadcast",
+        desc: "The adapter has not broadcast the transaction.",
+        test: (r) => r.wasBroadcast === false,
+        data: (r) => r.wasBroadcast,
+      },
+      {
+        name: "Returns Signature",
+        desc: "The wallet has returned a signature in the response.",
+        test: (r) => !!(r.transaction.signatures && r.transaction.signatures.length >= 1),
+        data: (r) => JSON.stringify(r.transaction.signatures),
+      },
+      {
+        name: "Returns Serialized Transaction",
+        desc: "The wallet has returned the serialized transaction appropriately.",
+        test: (r) => !!(r.transaction.serializedTransaction),
+        data: (r) => JSON.stringify(r.transaction.serializedTransaction),
+      },
+      {
+        name: "Transaction can be deserialized",
+        desc: "The serialized transaction that the wallet returned successfully deserialized.",
+        test: (r) => {
+          try {
+            const tx = this.deserialize(r.transaction.serializedTransaction)
+            return (
+              tx.expiration
+              && tx.actions
+              && tx.actions.length > 0
+              && tx.ref_block_num > 0
+              && tx.ref_block_prefix > 0
+            )
+          } catch (e) {
+            return false
+          }
+        },
+        data: (r) => {
+          try {
+            const tx = this.deserialize(r.transaction.serializedTransaction)
+            return JSON.stringify(tx)
+          } catch (e) {
+            return JSON.stringify({})
+          }
+        }
+      },
+    ]
+    return tests.map((testCase) => {
+      // console.log(testCase, testCase.test(response), response)
+      return {
+        ...testCase,
+        res: testCase.test(response),
+        val: testCase.data(response),
+      }
+    })
+  }
+  deserialize = (serialized) => {
+    const buffer = new Serialize.SerialBuffer({
+      array: serialized,
+    })
+    const type = transactionTypes.get('transaction')
+    return type.deserialize(buffer)
+  }
   render() {
     const { response } = this.props;
+    const tests = this.testResponse()
+    // console.log(tests)
     return (
       <React.Fragment>
-        <Segment style={{ background: 'rgb(39, 40, 34)' }}>
-          <Header dividing size="large" style={{ borderBottom: '1px solid white', color: 'white' }}>
-            Callback Payload
-          </Header>
-          <CodeBlock
-            text={`console.log(response.payload);`}
-            language={"javascript"}
-            showLineNumbers={false}
-            theme={monokai}
-            wrapLines
-          />
-          <ReactJson
-            displayDataTypes={false}
-            displayObjectSize={false}
-            enableClipboard={false}
-            iconStyle="square"
-            name={null}
-            src={response.payload}
-            style={{
-              background: 'rgb(39, 40, 34)',
-              marginTop: '1em',
-              overflow: 'scroll',
-            }}
-            theme="harmonic"
-          />
+        <Segment style={{ overflowX: 'scroll' }}>
+          <List divided relaxed>
+            {tests.map(test => (
+              <List.Item>
+                <List.Icon
+                  color={test.res ? "green" : "red"}
+                  name={test.res ? "checkmark" : "x"}
+                  size="large"
+                  verticalAlign="top"
+                />
+                <List.Content>
+                  <List.Header>{test.name}</List.Header>
+                  <List.Description>{test.desc}</List.Description>
+                  <List.Content style={{ margin: '0.5em 0' }}><code>{test.val}</code></List.Content>
+                </List.Content>
+              </List.Item>
+            ))}
+          </List>
         </Segment>
-        <Segment style={{ background: 'rgb(39, 40, 34)' }}>
-          <Header dividing size="large" style={{ borderBottom: '1px solid white', color: 'white' }}>
-            Signatures
-          </Header>
-          <CodeBlock
-            text={`console.log(response.signatures);`}
-            language={"javascript"}
-            showLineNumbers={false}
-            theme={monokai}
-            wrapLines
-          />
-          <ReactJson
-            displayDataTypes={false}
-            displayObjectSize={false}
-            enableClipboard={false}
-            iconStyle="square"
-            name={null}
-            src={response.signatures}
-            style={{
-              background: 'rgb(39, 40, 34)',
-              marginTop: '1em',
-              overflow: 'scroll',
-            }}
-            theme="harmonic"
-          />
-        </Segment>
-        <Segment style={{ background: 'rgb(39, 40, 34)' }}>
-          <Header dividing size="large" style={{ borderBottom: '1px solid white', color: 'white' }}>
-            Signer
-          </Header>
-          <CodeBlock
-            text={`console.log(response.signer);`}
-            language={"javascript"}
-            showLineNumbers={false}
-            theme={monokai}
-            wrapLines
-          />
-          <ReactJson
-            displayDataTypes={false}
-            displayObjectSize={false}
-            enableClipboard={false}
-            iconStyle="square"
-            name={null}
-            src={response.signer}
-            style={{
-              background: 'rgb(39, 40, 34)',
-              marginTop: '1em',
-              overflow: 'scroll',
-            }}
-            theme="harmonic"
-          />
-        </Segment>
-        <Segment style={{ background: 'rgb(39, 40, 34)' }}>
-          <Header dividing size="large" style={{ borderBottom: '1px solid white', color: 'white' }}>
-            Transaction
-          </Header>
-          <CodeBlock
-            text={`console.log(response.transaction);`}
-            language={"javascript"}
-            showLineNumbers={false}
-            theme={monokai}
-            wrapLines
-          />
-          <ReactJson
-            displayDataTypes={false}
-            displayObjectSize={false}
-            enableClipboard={false}
-            iconStyle="square"
-            name={null}
-            src={response.transaction}
-            style={{
-              background: 'rgb(39, 40, 34)',
-              marginTop: '1em',
-              overflow: 'scroll',
-            }}
-            theme="harmonic"
-          />
-        </Segment>
+
       </React.Fragment>
     );
   }
